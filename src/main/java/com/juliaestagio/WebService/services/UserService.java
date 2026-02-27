6e8a30ca6152982d172428be085daaf3a0698817
Juuuuh <ajuliagomes45@gmail.com>
2026-02-27 16:39:22 +0000

    Exception handling - findById

--- src/main/java/com/juliaestagio/WebService/services/UserService.java
+++ src/main/java/com/juliaestagio/WebService/services/UserService.java
@@ -2,6 +2,7 @@ package com.juliaestagio.WebService.services;
 
 import com.juliaestagio.WebService.entities.User;
 import com.juliaestagio.WebService.repositories.UserRepository;
+import com.juliaestagio.WebService.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert (User obj){
---
+++ src/main/java/com/juliaestagio/WebService/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.juliaestagio.WebService.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id "+ id);
+    }
+}
